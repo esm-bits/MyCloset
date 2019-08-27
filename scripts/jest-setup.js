08ffddffee6dfa4d67c28f5f1caa62f011b8726a
h-asakura <haruka.asakura@esm.co.jp>
2019-08-27 17:27:00 +0900

    refactor: defaultエクスポートを使わない

--- scripts/jest-setup.js
+++ scripts/jest-setup.js
@@ -3,7 +3,7 @@
  * グローバルでモックするモジュールなどもここに書く
  */
 
-import Enzyme from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
