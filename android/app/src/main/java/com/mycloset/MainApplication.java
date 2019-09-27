656208d5eda8039d4c708b851fc9b567190dacf4
h-asakura <haruka.asakura@esm.co.jp>
2019-09-27 13:40:29 +0900

    feat: Androidでもautolinkingを使う

--- android/app/src/main/java/com/mycloset/MainApplication.java
+++ android/app/src/main/java/com/mycloset/MainApplication.java
@@ -2,21 +2,17 @@ package com.mycloset;
 
 import androidx.annotation.Nullable;
 
-import com.avishayil.rnrestart.ReactNativeRestartPackage;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.masteratul.exceptionhandler.ReactNativeExceptionHandlerPackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.reactnativenavigation.react.ReactGateway;
 
-import org.reactnative.camera.RNCameraPackage;
-
-import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends NavigationApplication {
+public class MainApplication extends NavigationApplication implements ReactApplication {
     @Override
     protected ReactGateway createReactGateway() {
         ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
@@ -36,11 +32,14 @@ public class MainApplication extends NavigationApplication {
     @Nullable
     @Override
     public List<ReactPackage> createAdditionalReactPackages() {
-        return Arrays.asList(
-                new RNCameraPackage(),
-                new ReactNativeRestartPackage(),
-                new ReactNativeExceptionHandlerPackage(),
-                new AsyncStoragePackage()
-        );
+        return this.getPackages();
+    }
+
+    protected List<ReactPackage> getPackages() {
+        @SuppressWarnings("UnnecessaryLocalVariable")
+        List<ReactPackage> packages = new PackageList(this).getPackages();
+        // Packages that cannot be autolinked yet can be added manually here, for example:
+        // packages.add(new MyReactNativePackage());
+        return packages;
     }
 }
