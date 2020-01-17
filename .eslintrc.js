743267b6ddd90742f6a43881370cf174e0a74aee
h-asakura <haruka.asakura@esm.co.jp>
2020-01-17 13:08:07 +0900

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   root: true,
-  ignorePatterns: ['node_modules/', 'android/', 'ios'],
+  ignorePatterns: ['node_modules/', 'android/', 'ios/'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['eslint:recommended', 'plugin:prettier/recommended', '@typescript-eslint'],
+  plugins: ['jest'],
   rules: {
     'prettier/prettier': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
@@ -15,6 +15,8 @@ module.exports = {
     'react-native/no-inline-styles': 'warn',
   },
   extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
     '@react-native-community',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
