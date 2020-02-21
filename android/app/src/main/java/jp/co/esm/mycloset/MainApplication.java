3eb1c9c42a26279acebc716968cbcf0928e665d4
h-asakura <haruka.asakura@esm.co.jp>
2020-02-21 12:28:11 +0900

    fix

--- android/app/src/main/java/jp/co/esm/mycloset/MainApplication.java
+++ android/app/src/main/java/jp/co/esm/mycloset/MainApplication.java
@@ -1,50 +1,50 @@
 package jp.co.esm.mycloset;
 
+
+import android.app.Application;
 import android.content.Context;
-
-import androidx.annotation.Nullable;
-
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactnativenavigation.NavigationApplication;
-import com.reactnativenavigation.react.NavigationReactNativeHost;
-import com.reactnativenavigation.react.ReactGateway;
-
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-public class MainApplication extends NavigationApplication implements ReactApplication {
+public class MainApplication extends Application implements ReactApplication {
+
+    private final ReactNativeHost mReactNativeHost =
+            new ReactNativeHost(this) {
+                @Override
+                public boolean getUseDeveloperSupport() {
+                    return BuildConfig.DEBUG;
+                }
+
+                @Override
+                protected List<ReactPackage> getPackages() {
+                    @SuppressWarnings("UnnecessaryLocalVariable")
+                    List<ReactPackage> packages = new PackageList(this).getPackages();
+                    // Packages that cannot be autolinked yet can be added manually here, for example:
+                    // packages.add(new MyReactNativePackage());
+                    return packages;
+                }
+
+                @Override
+                protected String getJSMainModuleName() {
+                    return "index";
+                }
+            };
+
     @Override
-    protected ReactGateway createReactGateway() {
-        ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
-            @Override
-            protected String getJSMainModuleName() {
-                return "index";
-            }
-        };
-        return new ReactGateway(this, isDebug(), host);
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    public boolean isDebug() {
-        return BuildConfig.DEBUG;
-    }
-
-    @Nullable
-    @Override
-    public List<ReactPackage> createAdditionalReactPackages() {
-        return this.getPackages();
-    }
-
-    protected List<ReactPackage> getPackages() {
-        @SuppressWarnings("UnnecessaryLocalVariable")
-        List<ReactPackage> packages = new PackageList(this).getPackages();
-        // Packages that cannot be autolinked yet can be added manually here, for example:
-        // packages.add(new MyReactNativePackage());
-        return packages;
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
+        initializeFlipper(this); // Remove this line if you don't want Flipper enabled
     }
 
     /**
@@ -55,10 +55,10 @@ public class MainApplication extends NavigationApplication implements ReactAppli
     private static void initializeFlipper(Context context) {
         if (BuildConfig.DEBUG) {
             try {
-                /*
-                    We use reflection here to pick up the class that initializes Flipper,
-                    since Flipper library is not available in release mode
-                */
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
                 Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
                 aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
             } catch (ClassNotFoundException e) {
@@ -71,11 +71,5 @@ public class MainApplication extends NavigationApplication implements ReactAppli
                 e.printStackTrace();
             }
         }
-    }
-
-    public void onCreate() {
-        super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
-        initializeFlipper(this); // Remove this line if you don't want Flipper enabled
     }
 }
